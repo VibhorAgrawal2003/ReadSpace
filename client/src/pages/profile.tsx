36a96a2947bcc882582941bd0a4de75a2dfc4987
Vibhor <vibhorag2003@gmail.com>
2024-08-17 18:40:45 +0530

    feat: add customize profile link

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -17,17 +17,11 @@ const Profile: React.FC = () => {
         const fetchProfile = async () => {
             const token = sessionStorage.getItem("token");
 
-            {
-                /* Check if token in session */
-            }
             if (!token) {
                 navigate("/auth");
                 return;
             }
 
-            {
-                /* Try to fetch user data */
-            }
             try {
                 const response = await axios.get(`${import.meta.env.VITE_SERVER}/client/users/${username}`, {
                     headers: { Authorization: `Bearer ${token}` },
@@ -35,7 +29,6 @@ const Profile: React.FC = () => {
                 setProfile(response.data);
             } catch (error) {
                 console.error("Error fetching profile:", error);
-                navigate("/auth");
             }
         };
 
@@ -47,21 +40,36 @@ const Profile: React.FC = () => {
             <div className='flex items-center space-x-6'>
                 {/* User Image */}
                 <img
-                    src={profile?.picture_url}
+                    src={profile?.picture_url || `${import.meta.env.VITE_DEFAULT_PIC}`}
                     alt='User Avatar'
                     className='w-32 h-32 rounded-full object-cover border-4 border-gray-300'
                 />
 
                 {/* User Info */}
                 <div>
-                    <h1 className='text-4xl font-extrabold text-gray-800 text-left mb-2'>{profile?.username}</h1>
-                    <p className='text-lg text-gray-600 mb-4 text-left'>{profile?.bio}</p>
+                    <h1 className='text-4xl font-extrabold text-gray-800 text-left mb-2'>
+                        {profile?.username || "Anonymous"}
+                    </h1>
+                    <p className='text-lg text-gray-600 mb-4 text-left'>
+                        {profile?.bio || "This user does not exist."}
+                    </p>
+
+                    {username == sessionStorage.username && (
+                        <p
+                            className='text-sm text-gray-600 text-left cursor-pointer hover:text-blue-500'
+                            onClick={() => navigate("/customize")}
+                        >
+                            Customize Profile
+                        </p>
+                    )}
                 </div>
             </div>
 
             {/* List of user's blogs */}
             <div className='mt-6 p-4 border-t border-gray-200'>
-                <h2 className='text-2xl font-semibold text-gray-700 text-left'>Blogs by {profile?.username}</h2>
+                <h2 className='text-2xl font-semibold text-gray-700 text-left'>
+                    Blogs by {profile?.username || "Anonymous"}
+                </h2>
             </div>
         </div>
     );
